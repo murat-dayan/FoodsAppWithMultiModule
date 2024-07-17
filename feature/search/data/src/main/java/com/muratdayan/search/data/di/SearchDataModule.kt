c66e94a9cfbd2fa5b43c32b15a857de5864b6b34
muratdayan <muratdayan359@gmail.com>
2024-07-17 12:19:04 +0300

    search data module object created

---
+++ feature/search/data/src/main/java/com/muratdayan/search/data/di/SearchDataModule.kt
@@ -0,0 +1,32 @@
+package com.muratdayan.search.data.di
+
+import com.muratdayan.search.data.remote.SearchApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+const val BASE_URL = "https://www.themealdb.com/"
+
+@InstallIn(SingletonComponent::class)
+@Module
+object SearchDataModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit() : Retrofit{
+        return  Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun provideSearchApiService(retrofit: Retrofit): SearchApiService{
+        return  retrofit.create(SearchApiService::class.java)
+    }
+
+}
