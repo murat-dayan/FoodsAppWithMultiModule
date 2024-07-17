a0a207eee74cc975169af2de4b83ebe6a9d18944
muratdayan <muratdayan359@gmail.com>
2024-07-17 13:18:31 +0300

    SearchRepoImpl functions wrapped try-catch

--- feature/search/data/src/main/java/com/muratdayan/search/data/repository/SearchRepoImpl.kt
+++ feature/search/data/src/main/java/com/muratdayan/search/data/repository/SearchRepoImpl.kt
@@ -10,30 +10,38 @@ class SearchRepoImpl(
     private val searchApiService: SearchApiService
 ) : SearchRepository {
     override suspend fun getRecipes(s: String): Result<List<Recipe>> {
-        val response = searchApiService.getRecipes(s)
-        return if (response.isSuccessful) {
-            response.body()?.meals?.let {
-                Result.success(it.toDomain())
-            }?: run { Result.failure(Exception("error occured getrecipes in searchrepoimpl run block")) }
-        }else{
-            Result.failure(Exception("error occured getrecipes in searchrepoimpl"))
+        return try {
+            val response = searchApiService.getRecipes(s)
+            if (response.isSuccessful) {
+                response.body()?.meals?.let {
+                    Result.success(it.toDomain())
+                }?: run { Result.failure(Exception("error occured getrecipes in searchrepoimpl run block")) }
+            }else{
+                Result.failure(Exception("error occured getrecipes in searchrepoimpl"))
+            }
+        }catch (e:Exception){
+            Result.failure(e)
         }
 
     }
 
 
     override suspend fun getRecipeDetails(id: String): Result<RecipeDetails> {
-        val response = searchApiService.getRecipeDetails(id)
-        return if (response.isSuccessful) {
-            response.body()?.meals?.let {
-                if (it.isNotEmpty()) {
-                    Result.success(it.first().toDomain())
-                }else{
-                    Result.failure(Exception("error occured getrecipedetails in searchrepoimpl meals is empty"))
-                }
-            }?: run { Result.failure(Exception("error occured getrecipedetails in searchrepoimpl run block")) }
-        }else{
-            Result.failure(Exception("error occured getrecipedetails in searchrepoimpl"))
+        return try {
+            val response = searchApiService.getRecipeDetails(id)
+            if (response.isSuccessful) {
+                response.body()?.meals?.let {
+                    if (it.isNotEmpty()) {
+                        Result.success(it.first().toDomain())
+                    }else{
+                        Result.failure(Exception("error occured getrecipedetails in searchrepoimpl meals is empty"))
+                    }
+                }?: run { Result.failure(Exception("error occured getrecipedetails in searchrepoimpl run block")) }
+            }else{
+                Result.failure(Exception("error occured getrecipedetails in searchrepoimpl"))
+            }
+        }catch (e:Exception){
+            Result.failure(e)
         }
     }
 }
