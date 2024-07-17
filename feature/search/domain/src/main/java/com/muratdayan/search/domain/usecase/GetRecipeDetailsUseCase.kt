114e5ad6ea83b8519aede6440b9dd80f81fbc396
muratdayan <muratdayan359@gmail.com>
2024-07-17 13:16:26 +0300

    use cases classes is done

--- feature/search/domain/src/main/java/com/muratdayan/search/domain/usecase/GetRecipeDetailsUseCase.kt
+++ feature/search/domain/src/main/java/com/muratdayan/search/domain/usecase/GetRecipeDetailsUseCase.kt
@@ -1,7 +1,7 @@
 package com.muratdayan.search.domain.usecase
 
 import com.muratdayan.common.utils.NetworkResult
-import com.muratdayan.search.domain.model.Recipe
+import com.muratdayan.search.domain.model.RecipeDetails
 import com.muratdayan.search.domain.repository.SearchRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
@@ -9,21 +9,20 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
-class GetAllRecipeUseCase @Inject constructor(
+class GetRecipeDetailsUseCase @Inject constructor(
     private val searchRepository: SearchRepository
-) {
+){
 
-    operator fun invoke(s:String) = flow<NetworkResult<List<Recipe>>> {
+    operator fun invoke(id:String) = flow<NetworkResult<RecipeDetails>> {
         emit(NetworkResult.Loading())
-        val response = searchRepository.getRecipes(s)
+        val response = searchRepository.getRecipeDetails(id)
         if (response.isSuccess){
             emit(NetworkResult.Success(data = response.getOrThrow()))
         }else{
             emit(NetworkResult.Error(response.exceptionOrNull()?.localizedMessage))
         }
-    }.catch {
-        emit(NetworkResult.Error(it.message.toString()))
     }.flowOn(Dispatchers.IO)
-
-
+        .catch {
+            emit(NetworkResult.Error(it.message.toString()))
+        }
 }
